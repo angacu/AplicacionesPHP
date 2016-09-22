e79dc7b57ca82d76cb72966d9b802cc710061b3c
Andreu Garcia Cueva <angacu@floridauniversitaria.es>
2016-09-22 10:36:20 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+//fichero index.php
+//creado por Paco Gomez
+<html>
+  <head>
+      <title>ACTIVIDAD 03</title>
+      <?php
+        echo "Andreu;
+        echo "Garcia";
+        echo "Cuevas";
+      ?>
+  </head>
+  <body>
+</html>
+?>
